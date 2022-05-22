66213dee9a2f41a7b96df49ab0185833d47e57a3
Evgeniy Salish <92336813+jeka5650962@users.noreply.github.com>
2022-05-22 21:53:11 +0300

    Добавил импорты картинок в компоненты

--- src/components/Main/Content/Profile/Posts/PostProfileInfo/Photo/Photo.jsx
+++ src/components/Main/Content/Profile/Posts/PostProfileInfo/Photo/Photo.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import style from "./Photo.module.scss";
+import avaImg from "../../../ProfileInfo/ava.jpg";
 
 const Photo = () => {
     return (
         <div className={style.profileInfo__photo}>
-            <img
-                src="https://media-exp1.licdn.com/dms/image/C4D03AQGP5l7bQHqE6A/profile-displayphoto-shrink_200_200/0/1633726581090?e=1657756800&v=beta&t=fISZxc6Jmt1lC2p18tlw-FUiEIVCVNojRFLR98C8y_o"
-                alt="Avatar"/>
+            <img src={avaImg} alt="Avatar"/>
         </div>
     );
 };
--- src/components/Main/Content/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Main/Content/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import style from "./ProfileInfo.module.scss";
+import avaImg from "./ava.jpg";
 
 const ProfileInfo = () => {
     return (
@@ -7,9 +8,7 @@ const ProfileInfo = () => {
             <div className={style.profile__image}></div>
             <div className={style.profile__info}>
                 <div className={style.profile__avatar}>
-                    <img
-                        src="https://media-exp1.licdn.com/dms/image/C4D03AQGP5l7bQHqE6A/profile-displayphoto-shrink_200_200/0/1633726581090?e=1657756800&v=beta&t=fISZxc6Jmt1lC2p18tlw-FUiEIVCVNojRFLR98C8y_o"
-                        alt="Avatar"/>
+                    <img src={avaImg} alt="Avatar"/>
                 </div>
             </div>
             <div className={style.profile__text}>
