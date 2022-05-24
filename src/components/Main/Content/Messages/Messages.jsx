2ee0a7d19e34d01f77cebca3f020ab857edf484c
jeka5650962 <jeka5650962@gmail.com>
2022-05-24 11:57:59 +0300

    До перенастройки роутинга

--- src/components/Main/Content/Messages/Messages.jsx
+++ src/components/Main/Content/Messages/Messages.jsx
@@ -3,25 +3,41 @@ import style from "./Messages.module.scss";
 import MessagingPerson from "./MessagingPerson/MessagingPerson";
 import MessagingMessage from "./MessagingMessage/MessagingMessage";
 
+// ----------------------------------------------------------------------------- messagingPersonData
+let messagingPersonData = [
+    {
+        id: "kuzin",
+        name: "Kirill Kuzin",
+        date: "May 20",
+        urlPhoto: "https://media-exp1.licdn.com/dms/image/C4E03AQFkejP4VJ9jtw/profile-displayphoto-shrink_200_200/0/1580923016915?e=1658361600&v=beta&t=6BpIL7n1naN211Ijjrx0XvbFS7-iJQcbqKKojvHmjNg",
+        lastMessage: "Here is the last message preview... First",
+    },
+    {
+        id: "pul",
+        name: "Slava Pul",
+        date: "June 12",
+        urlPhoto: "https://media-exp1.licdn.com/dms/image/C5603AQH-C_MVqmnmvg/profile-displayphoto-shrink_200_200/0/1572968995389?e=1658361600&v=beta&t=Bb9-1k3qEYL-pv8RY0_3WAkEAq7SbQvJ2Qq8nJJNmYY",
+        lastMessage: "Here is the last message preview... Second",
+    },
+];
+// ---------------------------------------------------------------------------- messagingMessageData
+let messagingMessageData = [
+    {
+        message: "1. Cras accumsan accumsan hendrerit. Fusce feugiat metus dictum eros aliquet aliquam. Nulla facilisi. Proin euismod.",
+    },
+    {
+        message: "2. In ullamcorper commodo odio, at facilisis erat pellentesque et. Aliquam rutrum laoreet dui, ac commodo.",
+    },
+    {
+        message: "3. Morbi dictum vehicula ante bibendum semper. Praesent non nisi iaculis, efficitur neque non, tincidunt sem.",
+    },
+    {
+        message: "4. Suspendisse mattis commodo dignissim. Quisque consequat leo aliquet ex hendrerit, quis vehicula lectus gravida. Aliquam.",
+    },
+];
+
 const Messages = () => {
 
-    // ----------------------------------------------------------------------------- messagingPersonData
-    let messagingPersonData = [
-        {
-            id: "kuzin",
-            name: "Kirill Kuzin",
-            date: "May 20",
-            urlPhoto: "https://media-exp1.licdn.com/dms/image/C4E03AQFkejP4VJ9jtw/profile-displayphoto-shrink_200_200/0/1580923016915?e=1658361600&v=beta&t=6BpIL7n1naN211Ijjrx0XvbFS7-iJQcbqKKojvHmjNg",
-            lastMessage: "Here is the last message preview... First",
-        },
-        {
-            id: "pul",
-            name: "Slava Pul",
-            date: "June 12",
-            urlPhoto: "https://media-exp1.licdn.com/dms/image/C5603AQH-C_MVqmnmvg/profile-displayphoto-shrink_200_200/0/1572968995389?e=1658361600&v=beta&t=Bb9-1k3qEYL-pv8RY0_3WAkEAq7SbQvJ2Qq8nJJNmYY",
-            lastMessage: "Here is the last message preview... Second",
-        },
-    ];
     let messagingPersonElements = messagingPersonData.map(mpd =>
         <MessagingPerson
             id={mpd.id}
@@ -30,21 +46,7 @@ const Messages = () => {
             urlPhoto={mpd.urlPhoto}
             lastMessage={mpd.lastMessage}
         />);
-    // ---------------------------------------------------------------------------- messagingMessageData
-    let messagingMessageData = [
-        {
-            message: "1. Cras accumsan accumsan hendrerit. Fusce feugiat metus dictum eros aliquet aliquam. Nulla facilisi. Proin euismod.",
-        },
-        {
-            message: "2. In ullamcorper commodo odio, at facilisis erat pellentesque et. Aliquam rutrum laoreet dui, ac commodo.",
-        },
-        {
-            message: "3. Morbi dictum vehicula ante bibendum semper. Praesent non nisi iaculis, efficitur neque non, tincidunt sem.",
-        },
-        {
-            message: "4. Suspendisse mattis commodo dignissim. Quisque consequat leo aliquet ex hendrerit, quis vehicula lectus gravida. Aliquam.",
-        },
-    ];
+
     let messagingMessageElements = messagingMessageData.map(mmd =>
         <MessagingMessage
             message={mmd.message}
--- src/components/Main/Content/Profile/Profile.jsx
+++ src/components/Main/Content/Profile/Profile.jsx
@@ -4,23 +4,24 @@ import AddPost from "./AddPost/AddPost";
 import Post from "./Posts/Post";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
+// ---------------------------------------------------------------------------------------- postData
+let postData = [
+    {
+        postMessage: "First post. Quisque leo urna, vulputate eget ipsum non, dignissim elementum lectus.",
+        likesCount: 1988,
+    },
+    {
+        postMessage: "Second post. Aliquam feugiat vestibulum tortor, non sagittis sem tempus at. Nulla.",
+        likesCount: 2022,
+    },
+    {
+        postMessage: "Third post. Vestibulum maximus eu erat vel venenatis. Praesent mattis vulputate risus.",
+        likesCount: 8,
+    },
+];
+
 const Profile = () => {
 
-    // ---------------------------------------------------------------------------------------- postData
-    let postData = [
-        {
-            postMessage: "First post. Quisque leo urna, vulputate eget ipsum non, dignissim elementum lectus.",
-            likesCount: 1988,
-        },
-        {
-            postMessage: "Second post. Aliquam feugiat vestibulum tortor, non sagittis sem tempus at. Nulla.",
-            likesCount: 2022,
-        },
-        {
-            postMessage: "Third post. Vestibulum maximus eu erat vel venenatis. Praesent mattis vulputate risus.",
-            likesCount: 8,
-        },
-    ];
     let postElements = postData.map(pd =>
         <Post
             postMessage={pd.postMessage}
