dcbbe6b38f34d6191bbc7bd3858dbf4577e0f7bf
jeka5650962 <jeka5650962@gmail.com>
2022-05-27 10:04:40 +0300

    Вывод в alert текста из textarea

--- src/components/Main/Content/Profile/AddPost/AddPost.jsx
+++ src/components/Main/Content/Profile/AddPost/AddPost.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, {useRef} from "react";
 import style from "./AddPost.module.scss";
 
 const AddPost = () => {
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text);
+    };
+
+    let newPostElement = useRef(null);
+
     return (
         <div className={style.addPost}>
             <div className={style.addPost__title}>
                 Add new post
             </div>
             <div className={style.addPost__area}>
-                <textarea name="text"></textarea>
+                <textarea ref={newPostElement} name="text"></textarea>
             </div>
-            <div className={style.addPost__button}>Publish</div>
+            <button onClick={addPost} className={style.addPost__button}>Publish</button>
         </div>
     );
 };
