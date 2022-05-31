d62854c0fca772650670f5323a9b88a50b6adc18
Evgeniy Salish <jeka5650962@gmail.com>
2022-05-31 12:12:12 +0300

    Немного поправил роутинг

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,23 +2,20 @@ import React from "react";
 import style from "./Main.module.scss";
 import Sidebar from "./Sidebar/Sidebar";
 import Content from "./Content/Content";
-import {BrowserRouter as Router} from "react-router-dom";
 
 const Main = (props) => {
     return (
-        <Router>
-            <main className={style.main}>
-                <Sidebar/>
-                <Content
-                    friendsItemData={props.friendsItemData}
-                    messagingPersonData={props.messagingPersonData}
-                    messagingMessageData={props.messagingMessageData}
-                    postData={props.postData}
-                    addPost={props.addPost}
-                    addMessage={props.addMessage}
-                />
-            </main>
-        </Router>
+        <main className={style.main}>
+            <Sidebar/>
+            <Content
+                friendsItemData={props.friendsItemData}
+                messagingPersonData={props.messagingPersonData}
+                messagingMessageData={props.messagingMessageData}
+                postData={props.postData}
+                addPost={props.addPost}
+                addMessage={props.addMessage}
+            />
+        </main>
     );
 };
 
--- src/render.js
+++ src/render.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from "react-router-dom";
 import './index.scss';
 import App from './App';
 import {addMessage, addPost} from "./redux/state";
@@ -8,11 +9,13 @@ export let rerenderEntireTree = (state) => {
     const root = ReactDOM.createRoot(document.getElementById('root'));
     root.render(
         <React.StrictMode>
-            <App
-                state={state}
-                addPost={addPost}
-                addMessage={addMessage}
-            />
+            <BrowserRouter>
+                <App
+                    state={state}
+                    addPost={addPost}
+                    addMessage={addMessage}
+                />
+            </BrowserRouter>
         </React.StrictMode>
     );
 };
