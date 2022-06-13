d26441b8735bd34a72e79b8b4f60993ed16aad17
Evgeniy Salish <jeka5650962@gmail.com>
2022-06-13 13:29:06 +0300

    If else заменил на switch case

--- src/redux/messages-reducer.js
+++ src/redux/messages-reducer.js
@@ -2,18 +2,20 @@ const ADD_MESSAGE = 'ADD-MESSAGE';
 const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
 
 const messagesReducer = (state, action) => {
-
-    if (action.type === ADD_MESSAGE) {
-        let newMessage = {
-            message: state.newMessageText,
-        };
-        state.messagingMessageData.push(newMessage);
-        state.newMessageText = '';
-    } else if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
-        state.newMessageText = action.newText;
+    switch (action.type) {
+        case ADD_MESSAGE:
+            let newMessage = {
+                message: state.newMessageText,
+            };
+            state.messagingMessageData.push(newMessage);
+            state.newMessageText = '';
+            return state;
+        case UPDATE_NEW_MESSAGE_TEXT:
+            state.newMessageText = action.newText;
+            return state;
+        default:
+            return state;
     }
-
-    return state;
 };
 
 export default messagesReducer;
